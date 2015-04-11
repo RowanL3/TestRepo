9a6524129f21bf99a576851e33b11ff45ff99be9
Rowan Lochrin <Rowan@Rowans-MacBook-Air.local>
2015-04-11 13:46:04 -0700

    Added S_app.py

---
+++ S_app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+#from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/') 
+@app.route('/<name>')
+def index(name="Treehosue"):
+	return "Hello From {}".format(name)
+
+@app.route('/add/<int:num1>/<int:num2>')
+def add(num1,num2):
+	return '{} + {} = {}'.format(num1,num2,num1+num2)
+
+
+app.run(debug=True,port=8000,host='0.0.0.0')
